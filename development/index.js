a6f2fb91867036e2d593a0c28b8c7f45d0b51faa
rubonics <rubenrodriguez+github@gmail.com>
2018-06-19 15:56:12 -0400

    adds examples for render & removeFeatures
    + adds a blob of JSON that represents what the headless wordpress API might respond with
    + adds two map examples to the development index.html. The first shows how to render data to the map. The second shows how to render data, then remove the features that are produced by the render function.

--- development/index.js
+++ development/index.js
@@ -1,8 +1,53 @@
 var $ = require( 'jquery' )
 var map = require( '../src/index.js' )
+var apiData = require( './wordpress-api-sample.json' )
 
+// Wait for the Google Maps API to be load ( `window.google.maps` )
 $( window ).on('load', function () {
+
+  // Initialize a map without data to the default coordinates & zoom level
   map( {
-    container: document.querySelector( '.map' )
+    container: document.querySelector( '#map-default' )
   } ).render()
+
+  // Initialize a map with some markers, and default popup styling
+  var locationData = apiData.filter( apiDataHasLatLng ).map( apiDataToFeatureObject )
+  map( {
+    container: document.querySelector( '#map-data-added' )
+  } ).data( locationData )
+     .render()
+
+  // Remove features from the map after adding them
+  map( {
+    container: document.querySelector( '#map-data-removed' )
+  } ).data( locationData )
+     .render()
+     .removeFeatures()
 } )
+
+function apiDataHasLatLng ( entry ) {
+  return $.isPlainObject( entry.acf ) &&
+         $.isPlainObject( entry.acf.location_address ) &&
+         $.isNumeric( entry.acf.location_address.lat ) &&
+         $.isNumeric( entry.acf.location_address.lng )
+}
+
+function apiDataToFeatureObject ( entry ) {
+  return {
+    // `marker` object is passed into `google.maps.Marker`
+    marker: {
+      title: entry.acf.location_address.address,
+      position: {
+        lat: entry.acf.location_address.lat,
+        lng: entry.acf.location_address.lng,
+      },
+      // `infoWindow` object is passed into `google.maps.InfoWindow`
+      infoWindow: {
+        content: `<div class="info-window-content">
+          <p>${ entry.acf.location_address.address }</p>
+          ${ entry.acf.location_description }
+        </div>`
+      },
+    },
+  }
+}
