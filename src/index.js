a9eaab6172893c2f938dc71842df6afb3886f9cb
rubonics <rubenrodriguez+github@gmail.com>
2018-06-19 15:54:01 -0400

    adds render & removeFeatures methods
    + adds a render method to the map module that will render the current data array
    + adds a removeFeatures method to undo the feature additions made to the map during the render process
    + adds a Marker object that manages creation and removal of individual marker features. There is a basic map popup that comes along with this.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+/* globals google.maps */
+
 var deafultTileStyle = require( './tile-style.json' )
+var isObject = require( './utils/is-object.js' )
 var Marker = require( './marker.js' )
 
 module.exports = Map;
@@ -7,9 +10,16 @@ module.exports = Map;
 function Map ( options ) {
   if ( ! ( this instanceof Map ) ) return new Map( options )
 
+  // The element that contains to the Google Maps instance.
   var container = options.container || document.querySelector( '.map' )
   
+  // The global Google Maps instance for this initialization.
   var mapInstance = undefined;
+
+  // The data that is displayed on the map as different feature objects
+  var data = []
+  // The feature objects that have been made for the map, ie `google.maps.Marker`
+  var features;
 
   var opinionatedDefaultMapOptions = {
     // defaults to city zoom level
@@ -24,22 +34,84 @@ function Map ( options ) {
   var mapOptions = Object.assign( {}, opinionatedDefaultMapOptions, options )
 
   var api = {
+    instance: mapInstance,
     render: render,
-    data: data,
+    data: getSetData,
+    removeFeatures: removeFeatures,
   }
 
   return api;
 
   /**
-   * Render a map with the current mapOptions.
+   * - Render a map with the current `mapOptions`.
+   * - Create map `features` based on the current `data`.
+   *   - Currently supports rendering `marker` & `infoWindow` objects within `data`.
+   * - Set the map bounds to include the features that have been rendered.
    * 
-   * @return {object}  Reference to this modules API.
+   * @return {object} api  Reference to this module's API.
    */
   function render () {
-    if ( typeof mapInstance === 'undefined' ) {
+    if ( ! mapInstance ) {
       mapInstance = new google.maps.Map( container, mapOptions ) 
     }
+
+    if ( ! features ) {
+      features = data.filter( isObject )
+                     .filter( objectContainsKey( 'marker' ) )
+                     .map( returnObjectKey( 'marker' ) )
+                     .map( createMarker )
+    }
+
+    if ( isArrayWithItems( features ) ) {
+      var paddingInPixels = {
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50,
+      }
+      mapInstance.fitBounds( getFeatureBounds( features ), paddingInPixels )
+    }
+
     return api;
+
+    // Given options for a Marker, supply the underlying map instance,
+    // initialize the Marker & render it.
+    function createMarker ( markerOptions ) {
+      markerOptions = Object.assign( { map: mapInstance }, markerOptions )
+      return Marker( markerOptions ).render()
+    }
+
+    /**
+     * Given an array of map features, determine the bounding box that includes
+     * all of the points, and return the `bounds` object.
+     *
+     * bounds :  { north : Number, east : Number, south : Number, west : Number }
+     * 
+     * @param  {object} features  Map feature objects
+     * @return {object} bounds    Defines a box that includes all features
+     */
+    function getFeatureBounds ( features ) {
+      var bounds = { north: 0, east: 0, south: 0, west: 0 }
+      for (var i = 0; i < features.length; i++) {
+        var feature = features[ i ].instance()
+        if ( typeof feature.getPosition === 'function' ) {
+          var position = feature.getPosition()
+          if ( i === 0 ) {
+            bounds.north = position.lat()
+            bounds.south = position.lat()
+            bounds.east = position.lng()
+            bounds.west = position.lng()
+          }
+          else {
+            if ( position.lat() > bounds.north ) bounds.north = position.lat()
+            if ( position.lat() < bounds.south ) bounds.south = position.lat()
+            if ( position.lng() > bounds.east ) bounds.east = position.lng()
+            if ( position.lng() < bounds.west ) bounds.west = position.lng()
+          }
+        } 
+      }
+      return bounds;
+    }
   }
 
 
@@ -48,15 +120,47 @@ function Map ( options ) {
    * Given an array of data (or an object that gets turned into an array)
    * create the appropriate underlying representation for that data.
    *
-   * Currently supports & assumes that the desired outcome is to
-   * create a marker for each of the datum.
+   * `newData` should be an object or array that represents features
+   * to add to a map.
    * 
-   * @param  {?object} x [description]
-   * @return {[type]}   [description]
+   * @param  {?object} newData  An object or array of data to add to the map
+   * @return {object}  api      Reference to this module's API.
    */
-  function data ( x ) {
-    if ( ! arguments.length ) return mapInstance.data;
-    // process data
+  function getSetData ( newData ) {
+    if ( ! arguments.length ) return data;
+    if ( isObject( newData ) ) newData = [ newData ]
+    data = newData;
     return api;
   }
+
+  /**
+   * Remove all features from the map.
+   * 
+   * @return {object} api  Reference to this module's API
+   */
+  function removeFeatures () {
+    features.forEach( function ( feature ) {
+      feature.remove()
+    } )
+    features = null;
+    return api;
+  }
+}
+
+// --- Utilities ---
+
+function objectContainsKey ( key ) {
+  return function doesObjectContainKey ( obj ) {
+    return obj.hasOwnProperty( key )
+  }
+}
+
+function returnObjectKey ( key ) {
+  return function returnsKeyInObject ( obj ) {
+    return obj[ key ]
+  }
+}
+
+function isArrayWithItems ( value ) {
+  return value && Array.isArray( value ) && value.length > 0;
 }
--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,134 @@
+/* globals google.maps */
+
+var isObject = require( './utils/is-object.js' )
+
 module.exports = Marker;
 
+/**
+ * Create a google.maps.Marker with an optional google.maps.InfoWindow
+ * that opens when the marker is clicked.
+ * 
+ * @param {object} options
+ * @param {object} options.map  The google.maps.Map instance to create in. Required.
+ * @param {object} options.position     The position object of the marker. Required.
+ * @param {number} options.position.lat The latitude of the marker.
+ * @param {number} options.position.lng The longitude of the marker.
+ * @param {?object} options.infoWindow  Optional object to define a marker popup.
+ * @return {object} api  The methods available to the created marker.
+ */
 function Marker ( options ) {
   if ( ! ( this instanceof Marker ) ) return new Marker( options )
+
+  if ( ! validateOptions( options ) ) {
+    var errorMessage = 'Could not initialize Marker.' +
+      '\nMarker requires the following keys:' +
+      '\n\t- `map`: A google.maps.Map instance' +
+      '\n\t- `position`: An object that includes a `lat` & `lng` key of ' +
+      '\n\t              latitude & longitude values for the marker.' +
+      '\n\t              These keys can be supplied as floats or strings.'
+
+    console.log( errorMessage )
+    console.log( 'Was supplied with:' )
+    console.log( options )
+    return;
+  }
+
+  var marker;
+  var markerListeners = []
+  var infoWindow;
+
+  var api = {
+    instance: function () { return marker },
+    options: options,
+    render: render,
+    remove: remove,
+  }
+
+  return api;
+
+  /**
+   * Validate options expects an object that contains keys that
+   * will define a google.maps.Marker object.
+   *
+   * Additionally ensures that `lat` & `lng` values are floats
+   * & not strings.
+   * 
+   * @param  {object} markerOptions
+   * @return {object} markerOptions
+   */
+  function validateOptions ( markerOptions ) {
+    var hasPosition = isObject( markerOptions ) &&
+                      isObject( markerOptions.position )
+
+    if ( ! hasPosition ) return false;
+
+    markerOptions.position.lat = Number.parseFloat( markerOptions.position.lat )
+    markerOptions.position.lng = Number.parseFloat( markerOptions.position.lng )
+
+    var hasMap = markerOptions.hasOwnProperty( 'map' )
+
+    var hasLat = ! Number.isNaN( markerOptions.position.lat )
+    var hasLng = ! Number.isNaN( markerOptions.position.lng )
+
+    return hasPosition && hasMap && hasLat && hasLng;
+  }
+
+  /**
+   * Renders the marker to the map, and adds any event listeners
+   * that are defined in the marker options.
+   * 
+   * @return {object} api  A reference to the API.
+   */
+  function render () {
+    if ( ! marker ) {
+      marker = new google.maps.Marker( options )
+    }
+
+    if ( options.infoWindow && ! infoWindow ) {
+      infoWindow = new google.maps.InfoWindow( options.infoWindow )
+      var markerClick = marker.addListener( 'click', function () {
+        infoWindow.open( options.map, marker )
+      } )
+      markerListeners.push( markerClick )
+    }
+
+    if ( isObject( options.on ) && isEmptyArray( markerListeners ) ) {
+      Object.keys( options.on ).forEach( function ( eventType ) {
+        var eventHandler = options.on[ eventType ].bind( api )
+        var eventListenerReference = marker.addListener( eventType, eventHandler )
+        markerListeners.push( eventListenerReference )
+      } )
+    }
+
+    return api;
+  }
+
+  /**
+   * Remove the marker from the map, along with any listeners.
+   * Reset the internal values to their original state, such
+   * that `render` can reproduce them.
+   * 
+   * @return {object} This markers API.
+   */
+  function remove () {
+
+    marker.setMap( null )
+    markerListeners.forEach( removeListener )
+
+    marker = null;
+    infoWindow = null;
+    markerListeners = []
+
+    return api;
+
+    function removeListener ( listener ) {
+      listener.remove()
+    }
+  }
+}
+
+// --- Utilities ---
+
+function isEmptyArray ( value ) {
+  return value && Array.isArray( value ) && value.length === 0;
 }
